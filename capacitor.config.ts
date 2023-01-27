961ebec82c5d8bf37e0ec28e452615e1026f0450
Saravá Tech <saravatec@gmail.com>
2023-01-26 23:46:38 -0300

    tudo novo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'silvaTeamApp',
+  appId: 'com.cgtech.silvateam',
+  appName: 'Silva Team',
   webDir: 'www',
   bundledWebRuntime: false
 };
